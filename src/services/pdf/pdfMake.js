13f88dd8ccc736e7a606aff87e049fdfdf428c89
Vasilii <VVasiGoydin@sberban.ru>
2024-10-20 20:54:02 +0300

    FIX: исправление сборки vite

--- src/services/pdf/pdfMake.js
+++ src/services/pdf/pdfMake.js
@@ -1,8 +1,10 @@
+import * as pdfFonts from "./vfs_fonts"; // 👈 local import
 const pdfMakeImportFactory = () => import(
     /* webpackPrefetch: true */
     /* webpackChunkName: "pdfBuilder" */
     './pdfMakeModuleImpl.js'
     );
+
 
 let pdfMakeImport = null;
 
@@ -10,7 +12,19 @@ export default async function getPdfMake() {
 
     if (!pdfMakeImport) {
         pdfMakeImport = pdfMakeImportFactory();
+
+        pdfMakeImport.fonts = {
+            Roboto: {
+              normal:
+                "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+              bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+              italics:
+                "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+              bolditalics:
+                "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+            }
     }
+}
 
     const impl = await pdfMakeImport;
     return impl.default.pdfMake;
