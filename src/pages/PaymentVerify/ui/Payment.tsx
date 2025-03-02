1ea2ee27b3d1b0196cebe649b1e02017d2767d00
Vasyah <Souji_933@mail.ru>
2025-03-02 19:07:09 +0300

    feat: добавлено изменение свойств в puzzlebot api для корректной отправки уведомлений об оплате

--- src/pages/PaymentVerify/ui/Payment.tsx
+++ src/pages/PaymentVerify/ui/Payment.tsx
@@ -1,22 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { App, AutoCompleteProps, Button, ConfigProvider, Flex, List, Select, Spin } from 'antd';
-import { useListUsers, useUpdateUsersPayment } from '../../../shared/api/googleSheets';
-import { IUser } from '../../../features/UserList/types';
-import { CloseCircleFilled, SmileOutlined } from '@ant-design/icons';
-import { SEMINAR } from '../../../shared/utils/consts/consts';
-import { UserPaymentInfo } from './UserPaymentInfo';
-import { findUser } from '../lib/findUser';
-import { useMutation } from '@tanstack/react-query';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import {App, AutoCompleteProps, Button, ConfigProvider, Flex, List, Select, Spin} from 'antd';
+import {useListUsers, useUpdateUsersPayment} from '../../../shared/api/googleSheets';
+import {IUser} from '../../../features/UserList/types';
+import {CloseCircleFilled, SmileOutlined} from '@ant-design/icons';
+import {SEMINAR} from '../../../shared/utils/consts/consts';
+import {UserPaymentInfo} from './UserPaymentInfo';
+import {findUser} from '../lib/findUser';
+import {useMutation} from '@tanstack/react-query';
 import axios from 'axios';
-import { PuzzleBotApi } from 'app/config/puzzlebotApi';
+import {PuzzleBotApi} from 'app/config/puzzlebotApi';
 import cx from './style.module.scss';
+import {updatePaymentVariables} from "../../../shared/api/puzzleBot/hooks";
 
 export const Payment = () => {
-    const { message } = App.useApp();
+    const {message} = App.useApp();
     const usersRef = useRef(null);
 
-    const { mutate: sendPayment, isPending: isSending } = useMutation({
-        mutationFn: (user_id: string) => {
+    const {mutate: sendPayment, isPending: isSending} = useMutation({
+        mutationFn: (user_id: number) => {
             return axios.get(PuzzleBotApi.url, {
                 params: {
                     token: PuzzleBotApi.token,
@@ -30,34 +31,34 @@ export const Payment = () => {
 
     const getAutocompleteOption = (user: IUser) => {
         return {
-            label: <UserPaymentInfo user={user} />,
+            label: <UserPaymentInfo user={user}/>,
             value: user?.ФИО,
             ...user,
         };
     };
-    const { data, isLoading } = useListUsers();
-    const [options, setOptions] = useState<AutoCompleteProps['options']>([{}]);
+    const {data, isPending} = useListUsers();
+    // const [options, setOptions] = useState<AutoCompleteProps['options']>([{}]);
     const [selectedUsers, setSelectedUsers] = useState<IUser[]>([]);
     const [coordinator, setCoordinator] = useState<string | null>(null);
-    const { isUpdating, mutateAsync: updatePaymentsApi } = useUpdateUsersPayment(() => {
+    const {isUpdating, mutateAsync: updatePaymentsApi} = useUpdateUsersPayment(() => {
         message.success('Оплата подтверждена');
     });
+    const [query, setQuery] = useState('');
     const [open, setOpen] = useState(false);
 
-    useEffect(() => {
-        if (isLoading) return;
-
-        if (data) {
-            const userOptions = data?.map(getAutocompleteOption);
-            setOptions(userOptions);
+    const userOptions: AutoCompleteProps['options'] = useMemo(() => {
+        if (!!query) {
+            const filteredData = data?.filter((item) => String(item?.ФИО)?.toLowerCase().includes(query?.toLowerCase()));
+            return filteredData?.map(getAutocompleteOption);
         }
-    }, [data, isLoading]);
+        return data?.map(getAutocompleteOption) ?? [];
+    }, [data, query])
 
     useEffect(() => {
         if (open) {
             const preventScroll = (e) => e.preventDefault();
             document.body.style.overflow = 'hidden';
-            document.addEventListener('touchmove', preventScroll, { passive: false });
+            document.addEventListener('touchmove', preventScroll, {passive: false});
 
             return () => {
                 document.body.style.overflow = '';
@@ -66,8 +67,9 @@ export const Payment = () => {
         }
     }, [open]);
 
-    if (isLoading) {
-        return <Spin tip="Загрузка..." size="large" fullscreen />;
+
+    if (isPending) {
+        return <Spin tip="Загрузка..." size="large" fullscreen/>;
     }
 
     const onSelect = (_: string, user: IUser) => {
@@ -75,23 +77,21 @@ export const Payment = () => {
 
         if (idxUser >= 0) {
             message.warning(`${user?.ФИО} уже выбран`);
+            setQuery('')
             return;
         }
 
         setSelectedUsers((prev) => prev.concat(user));
+        setQuery('')
     };
     const customizeRenderEmpty = () => (
-        <div style={{ textAlign: 'center' }}>
-            <SmileOutlined style={{ fontSize: 20, marginTop: 20 }} />
+        <div style={{textAlign: 'center'}}>
+            <SmileOutlined style={{fontSize: 20, marginTop: 20}}/>
             <p>Участники для подтверждения оплаты не выбраны</p>
         </div>
     );
     const onSearch = (query: string) => {
-        if (!query) return;
-
-        const filteredData = data?.filter((item) => String(item?.ФИО)?.toLowerCase().includes(query?.toLowerCase()));
-        const userOptions = filteredData?.map(getAutocompleteOption);
-        setOptions(userOptions);
+        setQuery(query);
     };
 
     const onDeselect = (user: IUser) => {
@@ -111,34 +111,68 @@ export const Payment = () => {
         const usersToUpdate: IUser[] = users.map((user) => {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-expect-error
-            const { label, value, ...rest } = user;
+            const {label, value, ...rest} = user;
             return {
                 ...rest,
                 Координатор: coordinator,
                 'Сумма оплаты': '10500',
-                'Дата оплаты': new Date(),
+                'Дата оплаты': new Date().toLocaleDateString()
             };
         });
         try {
             await updatePaymentsApi(usersToUpdate);
 
-            const users_id = usersToUpdate.map((user) => user?.user_id);
 
-            users_id.forEach((user_id) =>
-                sendPayment(user_id, {
-                    onSuccess: () => {
-                        message.success('Уведомления отправлены в телеграм');
-                        setSelectedUsers([]);
-                    },
-                    onError: () => message.error('Ошибка при отправке уведомлений'),
-                }),
+            const variables = new Map<number, { variable: string, value: string }[]>()
+
+            const users_id = usersToUpdate.map((user) => {
+
+                variables.set(+user?.user_id, [{
+                    variable: 'app_payment_coordinator_public',
+                    value: user?.Координатор
+                }, {
+                    variable: 'app_payment_sum_public',
+                    value: user?.["Сумма оплаты"]
+                }, {variable: 'app_payment_date_public', value: user?.["Дата оплаты"]}])
+
+                return user?.user_id
+            });
+
+
+            users_id.forEach((user_id) => {
+                variables?.get(+user_id)?.forEach(async ({variable, value}) => {
+                    await updatePaymentVariables({
+                        variable,
+                        value,
+                        user_id: +users_id
+                    })
+                })
+            })
+
+            users_id.forEach((user_id) => {
+                    sendPayment(user_id, {
+                        onSuccess: () => {
+                            message.success('Уведомления отправлены в телеграм');
+
+                            setSelectedUsers([]);
+                        },
+                        onError: (e) => {
+                            message.error(`Ошибка при отправке уведомлений ${e.message}`);
+                        },
+                    })
+                }
             );
-            // await sendPayments(users_id[0]);
+
+
         } catch (e) {
+            console.error(e);
             message.error('Ошибка во время подтверждения оплаты, напишите в тех. поддержку @vasyahG (телеграм)');
         }
+
+
     };
 
+    const loading = isPending || isUpdating || isSending
     return (
         <ConfigProvider renderEmpty={customizeRenderEmpty}>
             <Flex vertical gap={'middle'} className={cx.container}>
@@ -148,11 +182,11 @@ export const Payment = () => {
                     open={open}
                     ref={usersRef}
                     label={'Выберите участников'}
-                    options={options}
-                    style={{ minWidth: '100%', maxWidth: '600px', minHeight: '40px', fontSize: '16px' }}
+                    options={userOptions}
+                    style={{minWidth: '100%', maxWidth: '600px', minHeight: '40px', fontSize: '16px'}}
                     onSelect={onSelect}
                     dropdownRender={(menu) => (
-                        <div onScroll={(e) => e.stopPropagation()} style={{ maxHeight: '300px', overflowY: 'auto' }}>
+                        <div onScroll={(e) => e.stopPropagation()} style={{maxHeight: '300px', overflowY: 'auto'}}>
                             {menu}
                         </div>
                     )}
@@ -166,12 +200,13 @@ export const Payment = () => {
                     value={null}
                     showSearch
                     optionFilterProp="ФИО"
+                    loading={loading}
                     filterSort={(optionA, optionB) => (optionA?.ФИО ?? '').toLowerCase().localeCompare((optionB?.ФИО ?? '').toLowerCase())}
-                    loading={isLoading || isUpdating || isSending}
                     onDropdownVisibleChange={(isOpen) => setOpen(isOpen)}
+                    onClear={() => setQuery('')}
                 />
                 <List
-                    loading={isLoading || isUpdating || isSending}
+                    loading={loading}
                     style={{
                         minHeight: 'calc(100vh - 300px)',
                         maxHeight: 'calc(100vh - 300px)',
@@ -184,14 +219,19 @@ export const Payment = () => {
                         return (
                             <List.Item
                                 key={`${user?.user_id}${user?.ФИО}`}
-                                style={{ padding: '0.5rem 0.5rem' }}
+                                style={{padding: '0.5rem 0.5rem'}}
                                 actions={[
                                     <div key={0} onClick={() => onDeselect(user)}>
-                                        <CloseCircleFilled style={{ padding: '0.5rem', fontSize: '18px', color: 'red' }} />
+                                        <CloseCircleFilled style={{
+                                            padding: '0.5rem',
+                                            fontSize: '18px',
+                                            color: 'red',
+                                            cursor: 'pointer'
+                                        }}/>
                                     </div>,
                                 ]}
                             >
-                                <List.Item.Meta title={<UserPaymentInfo user={user} />} />
+                                <List.Item.Meta title={<UserPaymentInfo user={user}/>}/>
                             </List.Item>
                         );
                     }}
@@ -199,12 +239,11 @@ export const Payment = () => {
                 <Select
                     size={'large'}
                     placeholder={'Выберите координатора'}
-                    style={{ width: '100%', fontSize: '16px!important' }}
+                    style={{width: '100%', fontSize: '16px!important'}}
                     onChange={(coordinator) => setCoordinator(coordinator)}
                     dropdownStyle={{
                         fontSize: '16px',
                     }}
-                    // options={SEMINAR.COORDINATORS}
                 >
                     {SEMINAR.COORDINATORS.map((coordinator) => (
                         <Select.Option className={cx.option} key={coordinator.value} value={coordinator.value}>
@@ -213,10 +252,12 @@ export const Payment = () => {
                     ))}
                 </Select>
 
-                <Button type={'primary'} onClick={() => updatePayments(selectedUsers)} disabled={!selectedUsers?.length || !coordinator} loading={isLoading || isUpdating || isSending}>
+                <Button type={'primary'} onClick={() => updatePayments(selectedUsers)}
+                        loading={loading} disabled={!selectedUsers?.length || !coordinator}>
                     Подтвердить оплату
                 </Button>
             </Flex>
         </ConfigProvider>
-    );
+    )
+        ;
 };
--- src/shared/api/puzzleBot/hooks.ts
+++ src/shared/api/puzzleBot/hooks.ts
@@ -1,28 +1,8 @@
 import axios from 'axios';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { PuzzleBotApi } from '../../../app/config/puzzlebotApi';
-import { showMessage } from 'shared/components/message';
+import {PuzzleBotApi} from '../../../app/config/puzzlebotApi';
+import {showMessage} from 'shared/components/message';
 
-export function useSendPayments() {
-    const queryClient = useQueryClient();
 
-    const mutation = useMutation({
-        mutationFn: async (users_id: string[]) => {
-            return await sendPaymentsSuccess(users_id);
-        },
-        onSuccess: () => {
-            showMessage('success', 'Сообщения в телеграм отправлены');
-        },
-    });
-
-    return {
-        ...mutation,
-        isUpdating: mutation.isPending,
-    };
-}
-
-/*name
-    :*/
 // "Поздравление с успешной оплатой"
 export const sendPaymentsSuccess = async (users_id: string[]) => {
     try {
@@ -31,6 +11,7 @@ export const sendPaymentsSuccess = async (users_id: string[]) => {
         showMessage('error', ' Ошибка при отправке увеомления в телеграм');
     }
 };
+
 export const sendPaymentSuccess = (user_id: string) => {
     try {
         return axios
@@ -41,13 +22,33 @@ export const sendPaymentSuccess = (user_id: string) => {
                     command_name: 'Поздравление с успешной оплатой',
                     tg_chat_id: user_id,
                 },
-                // headers: {
-                //     // "Content-Type": "text/plain",
-                //     'Access-Control-Allow-Origin': '*'
-                // }
             })
-            .then((r) => r.data as string);
     } catch (error) {
         console.error(error);
     }
 };
+
+export const updatePaymentVariables = ({variable, user_id, value}: {
+        variable: string,
+        value: string | Date | number,
+        user_id: number
+    }) => {
+        try {
+            return axios
+                .get(PuzzleBotApi.url, {
+                        params: {
+                            token: PuzzleBotApi.token,
+                            method: 'variableChange',
+                            user_id: +user_id,
+                            variable: variable,
+                            expression: `"${value}"`
+
+                        }
+                    }
+                )
+        } catch
+            (error) {
+            console.error(error);
+        }
+    }
+;
