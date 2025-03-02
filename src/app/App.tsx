50826f1c8b3fce846bde53b277f61b98db4ad12e
Vasyah <Souji_933@mail.ru>
2025-03-02 11:46:05 +0300

    feat: add Telegram Api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import { AppLayout } from './provider/Layout/ui/AppLayout';
 import { AppRouter } from './provider/Router/ui/AppRouter';
 import { App as AntApp } from 'antd';
 export const App = () => {
+    useLayoutEffect(() => {
+        console.log(window.Telegram.WebApp);
+
+        if (window?.Telegram?.WebApp) {
+            const tg = window?.Telegram?.WebApp;
+
+            tg.isFullscreen = true;
+            tg.isVerticalSwipesEnabled = true;
+        }
+    }, []);
+
     return (
         <div className="App">
             <AntApp message={{ top: 50, duration: 1.5 }}>
