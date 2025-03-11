6611fe7f969c132393641e818b7d228017b76bf4
Vasyah <Souji_933@mail.ru>
2025-03-11 10:07:40 +0300

    feat: Vercel CORS

--- src/shared/api/googleSheets/hooks.ts
+++ src/shared/api/googleSheets/hooks.ts
@@ -56,7 +56,6 @@ const updateUserAccomodation = (accomodation: UserAccomodation) => {
             .post(SEMINAR.URL, accomodation, {
                 headers: {
                     'content-type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*',
                 },
             })
             .then((r) => r.data);
@@ -107,3 +106,4 @@ const updateUsersPayment = (users: IUser[]) => {
         console.error(error);
     }
 };
+
